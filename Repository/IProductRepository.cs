7aafb503910484dbe2fe46c83b0dc759dd043462
Olabamiji Oyetubo <olabamiji.oyetubo@sterling.ng>
2022-09-20 13:43:23 +0100

    new commit

---
+++ Repository/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProductMicroservice.Model;
+
+namespace ProductMicroservice.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+
+       Product GetProductByID(int ProductId);
+
+       void InsertProduct(Product product);
+
+        void UpdateProduct(Product product);
+
+        void DeleteProduct(int ProductId);
+
+        void Save();
+    }
+}
---
+++ Repository/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductMicroservice.DBContexts;
+using ProductMicroservice.Model;
+
+namespace ProductMicroservice.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _dbContext;
+
+        public ProductRepository(ProductContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void DeleteProduct(int ProductId)
+        {
+            var product = _dbContext.Products.Find(ProductId);
+            _dbContext.Products.Remove(product);
+            Save();
+        }
+
+        public Product GetProductByID(int ProductId)
+        {
+            return _dbContext.Products.Find(ProductId);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public void InsertProduct(Product product)
+        {
+            _dbContext.Add(product);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _dbContext.Entry(product).State = EntityState.Modified;
+            Save();
+        }
+    }
+}
